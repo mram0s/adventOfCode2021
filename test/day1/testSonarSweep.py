c1d8e6decfe07797d1cbcf608d17da50f8fe6f3a
Mariana Ramos <mariana.r1@thoughtworks.com>
2021-12-06 22:25:26 -0300

    day1: sonar sweep

---
+++ test/day1/testSonarSweep.py
@@ -0,0 +1,18 @@
+import unittest
+from unittest.mock import patch, mock_open
+from src.day1.sonarSweep import SonarSweep
+
+
+class TestSonarSweep(unittest.TestCase):
+    def test_should_return_the_increase_amount(self):
+        report = [188, 192, 193, 194, 192, 213, 214]
+        self.assertEqual(SonarSweep.get_increases_amount(report), 5)
+    
+
+    def test_should_return_the_sliding_window_increase_amount(self):
+        report = [188, 192, 193, 194, 192, 213, 214]
+        self.assertEqual(SonarSweep.get_sliding_window_increases_amount(report), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
